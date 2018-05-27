6bd0eb92ab886b2ad3ea2bf9aad2fcc17dfe2ec1
Alexander Sattelmaier <alexander@sattelmaier.info>
2018-05-27 10:34:03 +0200

    removed var_dump

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
 <?php
   $aufgabenlisteUrl = 'http://' . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) . '/aufgabenliste.php';
-  var_dump($aufgabenlisteUrl);
+
   header('Location: ' . $aufgabenlisteUrl);
   exit();
 ?>
